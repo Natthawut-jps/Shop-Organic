62d8b4e236f514bac6b5ef35505045f211a2d356
Natthawut Jampasaeng <121080181+Natthawut-jps@users.noreply.github.com>
2024-04-08 16:48:01 +0700

    up

--- src/pages/unities/axios_instance.tsx
+++ src/pages/unities/axios_instance.tsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: 'http://localhost:8080',
-  // baseURL: "https://y7ly4z-8080.csb.app",
+  // baseURL: 'http://localhost:8080',
+  baseURL: "https://api-shoporganic-ecommerce.onrender.com",
   timeout: 1000,
 });
 
